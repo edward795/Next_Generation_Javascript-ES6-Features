682738f0f94ecac88f98d426c0f5c800dbbc2961
edward795 <ayyappadasgb777999@gmail.com>
2022-03-31 13:10:13 +0530

    merge after generators module second

--- Asynchronous Actions/async and await.js
+++ Asynchronous Actions/async and await.js
@@ -1,76 +1,36 @@
-<<<<<<< HEAD
-const posts=[
-    {title:'Post One',body:'This is Post One'},
-    {title:'Post Two',body:'This is Post Two'}
-]
+const posts = [
+  { title: "Post One", body: "This is Post One" },
+  { title: "Post Two", body: "This is Post Two" },
+];
 
-function getPosts(){
-    let output='';
-    setTimeout(()=>{
-       posts.forEach((post,index)=>{
-           output+=`<li>${post.title}</li>`;
-       })
-       document.body.innerHTML=output;
-    },2000)
+function getPosts() {
+  let output = "";
+  setTimeout(() => {
+    posts.forEach((post, index) => {
+      output += `<li>${post.title}</li>`;
+    });
+    document.body.innerHTML = output;
+  }, 2000);
 }
 
-function createPost(post){
-    setTimeout(()=>{
-        posts.push(post);
-    },2000);
+function createPost(post) {
+  setTimeout(() => {
+    posts.push(post);
+  }, 2000);
 }
 
-async function init(){
-    await createPost({title:'Post Three',body:'This is post Three'});
-    getPosts()
-}
-
-init();
-
-
-//using fetch() api 
-async function fetchUsers(){
-    const res=await fetch('https://jsonplaceholder.typicode.com/users');
-    const data=await res.json() 
-    console.log(data)
-}
-
-=======
-const posts=[
-    {title:'Post One',body:'This is Post One'},
-    {title:'Post Two',body:'This is Post Two'}
-]
-
-function getPosts(){
-    let output='';
-    setTimeout(()=>{
-       posts.forEach((post,index)=>{
-           output+=`<li>${post.title}</li>`;
-       })
-       document.body.innerHTML=output;
-    },2000)
-}
-
-function createPost(post){
-    setTimeout(()=>{
-        posts.push(post);
-    },2000);
-}
-
-async function init(){
-    await createPost({title:'Post Three',body:'This is post Three'});
-    getPosts()
+async function init() {
+  await createPost({ title: "Post Three", body: "This is post Three" });
+  getPosts();
 }
 
 init();
 
-
-//using fetch() api 
-async function fetchUsers(){
-    const res=await fetch('https://jsonplaceholder.typicode.com/users');
-    const data=await res.json() 
-    console.log(data)
+//using fetch() api
+async function fetchUsers() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+  console.log(data);
 }
 
->>>>>>> 050748eb71941982df9a9b9dd3abaae2aec9579b
 fetchUsers();
--- Asynchronous Actions/callbacks.js
+++ Asynchronous Actions/callbacks.js
@@ -1,52 +1,25 @@
-<<<<<<< HEAD
-const posts=[
-    {title:'Post One',body:'This is Post One'},
-    {title:'Post Two',body:'This is Post Two'}
-]
+const posts = [
+  { title: "Post One", body: "This is Post One" },
+  { title: "Post Two", body: "This is Post Two" },
+];
 
-function getPosts(){
-    setTimeout(()=>{
-        let output='';
-        posts.forEach((post,index)=>{
-            output+=`<li>${post.title}</li>`
-        })
-        document.body.innerHTML=output;
-    },1000);
+function getPosts() {
+  setTimeout(() => {
+    let output = "";
+    posts.forEach((post, index) => {
+      output += `<li>${post.title}</li>`;
+    });
+    document.body.innerHTML = output;
+  }, 1000);
 }
 
 //Function which accepts a post and a callback function
-function createPosts(post,callback){
-  setTimeout(()=>{
-      posts.push(post);
-      callback()
-  },2000);  
+function createPosts(post, callback) {
+  setTimeout(() => {
+    posts.push(post);
+    callback();
+  }, 2000);
 }
 
-getPosts()
-=======
-const posts=[
-    {title:'Post One',body:'This is Post One'},
-    {title:'Post Two',body:'This is Post Two'}
-]
-
-function getPosts(){
-    setTimeout(()=>{
-        let output='';
-        posts.forEach((post,index)=>{
-            output+=`<li>${post.title}</li>`
-        })
-        document.body.innerHTML=output;
-    },1000);
-}
-
-//Function which accepts a post and a callback function
-function createPosts(post,callback){
-  setTimeout(()=>{
-      posts.push(post);
-      callback()
-  },2000);  
-}
-
-getPosts()
->>>>>>> 050748eb71941982df9a9b9dd3abaae2aec9579b
-createPosts({title:'Post Three',body:'This is post Three'},getPosts)
+getPosts();
+createPosts({ title: "Post Three", body: "This is post Three" }, getPosts);
